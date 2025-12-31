f965bda12606a02342e94cc63a2c5eccfaf0e17f
DexsDevelopers <162776282+DexsDevelopers@users.noreply.github.com>
2025-12-31 02:30:03 -0300

    refactor: remove todas as referências e arquivos relacionados à SumUp

--- admin/templates/header_admin.php
+++ admin/templates/header_admin.php
@@ -369,10 +369,6 @@ require_once '../config.php';
                     <i class="fas fa-qrcode w-5"></i>
                     <span>Gerenciar PIX</span>
                 </a>
-                <a href="gerenciar_sumup.php" class="admin-nav-item flex items-center gap-3 px-4 py-3 rounded-lg text-admin-gray-300 hover:text-white <?= basename($_SERVER['PHP_SELF']) == 'gerenciar_sumup.php' ? 'active' : '' ?>">
-                    <i class="fas fa-credit-card w-5"></i>
-                    <span>Gerenciar SumUp</span>
-                </a>
                 
                 <div class="border-t border-admin-gray-700 my-4"></div>
                 
